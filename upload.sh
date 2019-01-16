a0acbd4225c15aaf4181cd8abf9d52dab0d8fb2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-16 01:39:23 +0100

    Install redo and requests (temporary hack)

--- upload.sh
+++ upload.sh
@@ -2,5 +2,8 @@
 
 set -v -e -x
 
+# TODO: We shouldn't install these at runtime, but in the docker image.
+pip install redo
+pip install requests
 wget https://index.taskcluster.net/v1/task/project.socorro.mac-update-symbols.latest/artifacts/public/build/target.crashreporter-symbols.zip
 python upload_symbols.py target.crashreporter-symbols.zip
