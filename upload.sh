4267a6dd501dccb86def7da2391d85818ccad8f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-16 15:46:51 +0100

    Don't return error code if the zip file doesn't exist

--- upload.sh
+++ upload.sh
@@ -7,4 +7,6 @@ pip install redo
 pip install requests
 
 URL=https://queue.taskcluster.net/v1/task/${ARTIFACT_TASKID}/artifacts/public/build/target.crashreporter-symbols.zip
-wget -q --method HEAD "${URL}" && python upload_symbols.py ${URL}
+if wget --spider "${URL}"; then
+  python upload_symbols.py ${URL}
+fi
