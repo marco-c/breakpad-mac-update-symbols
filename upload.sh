2eb1b480b9c931ede76a78eb0cb6ef004a83c2c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-01-16 01:53:32 +0100

    Don't download from latest task, but from task specified by the decision task

--- upload.sh
+++ upload.sh
@@ -5,5 +5,6 @@ set -v -e -x
 # TODO: We shouldn't install these at runtime, but in the docker image.
 pip install redo
 pip install requests
-wget https://index.taskcluster.net/v1/task/project.socorro.mac-update-symbols.latest/artifacts/public/build/target.crashreporter-symbols.zip
+
+wget https://queue.taskcluster.net/v1/task/${ARTIFACT_TASKID}/artifacts/public/build/target.crashreporter-symbols.zip
 python upload_symbols.py target.crashreporter-symbols.zip
